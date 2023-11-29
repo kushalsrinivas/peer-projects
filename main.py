18ee37860ee24e5c2ea6d4ec7fcfa992df9191c9
kushal srinivas <49263417+kushalsrinivas@users.noreply.github.com>
2023-11-29 08:40:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import pyqrcode
 from datetime import datetime
 
 url = input("enter the url  : ")
-print(f"{url} is a valid domain.")
-print("generating qr code ....")
+print(f"generating qr code for {url} .... ")
+
 today_date = datetime.now().date()
 qr = pyqrcode.create(url)
 qr.png(f"{today_date}.png", scale=8)
-print(f"{url} is not a valid domain.")
+
